f66863d5ab2da0cb6bf206a7393a43cf29e441cd
Lu Tang <lutang@Lus-MacBook-Pro.hitronhub.home>
2020-07-02 15:06:22 -0700

    create a new cluster and then change MongoDB connection

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 // mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
-mongoose.connect('mongodb+srv://admin-lu:0629*Salu@cluster0-igwj0.mongodb.net/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+mongoose.connect('mongodb+srv://blogUser:0629*Salu@cluster0-igwj0.mongodb.net/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const postSchema = {
   title: String,
